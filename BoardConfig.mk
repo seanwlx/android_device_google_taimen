ca413f6fd7653b30fab6e988bd03a536df15ba48
seanwlx <sean.wlx@gmail.com>
2023-02-16 15:58:38 -0800

    Add selinux=permissive flag

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -61,6 +61,7 @@ BOARD_KERNEL_CMDLINE += loop.max_part=7
 BOARD_KERNEL_CMDLINE += raid=noautodetect
 BOARD_KERNEL_CMDLINE += usbcore.autosuspend=7
 BOARD_KERNEL_CMDLINE += androidboot.dtbo_idx=12
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 BOARD_KERNEL_BASE        := 0x00000000
 BOARD_KERNEL_PAGESIZE    := 4096
